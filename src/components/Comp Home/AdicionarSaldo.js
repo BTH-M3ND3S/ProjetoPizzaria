ac19c544845c9314405d8ff8109103087f00ceb1
BTH-M3ND3S <mendessilveriomateus@gmail.com>
2024-03-24 11:26:02 -0300

    adição do fundo nos comp home

--- src/components/Comp Home/AdicionarSaldo.js
+++ src/components/Comp Home/AdicionarSaldo.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, Button, StyleSheet, TouchableOpacity, Image } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default  function AdicionarSaldo( {handle} ) {
   return (
     <View style={styles.Container}>
+      <Image source={require('../Comp Você/imagebg.png')} style={styles.backgroundImage} />
         <TouchableOpacity onPress={() => handle(false)} style={{ position: 'absolute', top: 20, left: 20 }}>
         <View style={{ backgroundColor: 'red', borderRadius: 50, width: 40, height: 40, justifyContent: 'center', alignItems: 'center' }}>
           <Icon name="arrow-left" size={20} color="white" />
@@ -23,5 +24,12 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%', 
     backgroundColor: 'white',
-  }
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  },
 })
