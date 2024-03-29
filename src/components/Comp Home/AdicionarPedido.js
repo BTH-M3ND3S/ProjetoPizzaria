d912c74e6b90e81fb73f0ff80f494e9c538c35ed
BTH-M3ND3S <mendessilveriomateus@gmail.com>
2024-03-29 10:51:12 -0300

    criação de comps home

--- src/components/Comp Home/AdicionarPedido.js
+++ src/components/Comp Home/AdicionarPedido.js
@@ -1,36 +1,218 @@
-import React from 'react'
-import { View, Text, Button, StyleSheet, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+  FlatList,
+  Dimensions,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-export default  function AdicionarPedido( {handle} ) {
+const { width, height } = Dimensions.get('window');
+
+const menus = [
+  {
+    category: 'Pizzas',
+    items: [
+      { name: 'Mussarela', price: 15.99 },
+      { name: 'Calabresa', price: 18.99 },
+      { name: 'Peperoni', price: 17.99 },
+      { name: 'Margherita', price: 16.99 },
+      { name: 'Frango com Catupiry', price: 19.99 },
+    ],
+  },
+  {
+    category: 'Bebidas',
+    items: [
+      { name: 'Refrigerante', price: 5.99 },
+      { name: 'Suco Natural', price: 7.99 },
+      { name: 'Água Mineral', price: 3.99 },
+    ],
+  },
+  // Adicione outros menus aqui conforme necessário
+];
+
+export default function AdicionarPedido({ handle}) {
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  const handleAddToCart = (item) => {
+    setSelectedItems((prevItems) => [...prevItems, item]);
+  };
+
+  const handleRemoveFromCart = (index) => {
+    setSelectedItems((prevItems) => {
+      const newItems = [...prevItems];
+      newItems.splice(index, 1);
+      return newItems;
+    });
+  };
+
+  const renderMenuItem = ({ item, index }) => (
+    <View style={styles.menuItem}>
+      <Text style={styles.itemName}>{item.name}</Text>
+      <Text style={styles.itemPrice}>R$ {item.price.toFixed(2)}</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={() => handleAddToCart(item)}>
+          <Text style={styles.addButton}>+</Text>
+        </TouchableOpacity>
+        {selectedItems.some((i) => i === item) && (
+          <TouchableOpacity onPress={() => handleRemoveFromCart(index)}>
+            <Text style={styles.removeButton}>-</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
+
+  const renderMenuCategory = ({ item }) => (
+    <View style={styles.menuCategory}>
+      <Text style={styles.categoryName}>{item.category}</Text>
+      <FlatList
+        data={item.items}
+        renderItem={renderMenuItem}
+        keyExtractor={(item) => item.name}
+      />
+    </View>
+  );
+
+  const calculateTotal = () =>
+    selectedItems.reduce((acc, item) => acc + item.price, 0);
+
   return (
-    <View style={styles.Container}>
-        <Image source={require('../Comp Você/imagebg.png')} style={styles.backgroundImage} />
-        <TouchableOpacity onPress={() => handle(false)} style={{ position: 'absolute', top: 20, left: 20 }}>
+    <SafeAreaView style={styles.container}>
+      <Image source={require('../Comp Você/imagebg.png')} style={styles.backgroundImage} />
+      <View style={styles.content}>
+        <FlatList
+          data={menus}
+          renderItem={renderMenuCategory}
+          keyExtractor={(item) => item.category}
+        />
+        {selectedItems.length > 0 && (
+          <View style={styles.cart}>
+            <Text style={styles.cartTitle}>Seu Pedido:</Text>
+            <FlatList
+  data={selectedItems}
+  renderItem={({ item }) => (
+    <View style={styles.cartItem} key={selectedItems.indexOf(item)}>
+      <Text style={styles.cartItemName}>{item.name}</Text>
+      <Text style={styles.cartItemPrice}>R$ {item.price.toFixed(2)}</Text>
+    </View>
+  )}
+  keyExtractor={(item, index) => index.toString()} 
+/>
+            <Text style={styles.cartTotal}>Total: R$ {calculateTotal().toFixed(2)}</Text>
+            <Button title="Finalizar Pedido" />
+          </View>
+        )}
+      </View>
+      <TouchableOpacity onPress={() => handle(false)} style={{ position: 'absolute', top: 20, left: 20 }}>
         <View style={{ backgroundColor: 'red', borderRadius: 50, width: 40, height: 40, justifyContent: 'center', alignItems: 'center' }}>
           <Icon name="arrow-left" size={20} color="white" />
         </View>
       </TouchableOpacity>
-    </View>
-  )
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-  Container: {
+  container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
+    position: "absolute",
     width: '100%',
     height: '100%', 
-    backgroundColor: 'white',
   },
   backgroundImage: {
     flex: 1,
     resizeMode: 'cover',
-    position: 'absolute',
     width: '100%',
     height: '100%',
+    position: 'absolute',
   },
-
-})
+  content: {
+    flex: 1,
+    paddingTop: 20,
+    paddingHorizontal: 20,
+  },
+  menuCategory: {
+    marginBottom: 20,
+  },
+  categoryName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: 'white',
+  },
+  menuItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#fff',
+  },
+  itemName: {
+    fontSize: 16,
+    color: 'white',
+  },
+  itemPrice: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  addButton: {
+    fontSize: 20,
+    color: 'white',
+    marginLeft: 10,
+  },
+  removeButton: {
+    fontSize: 20,
+    color: 'white',
+    marginLeft: 10,
+  },
+  cart: {
+    marginTop: 20,
+    borderTopWidth: 1,
+    borderTopColor: '#fff',
+    paddingTop: 10,
+  },
+  cartTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: 'white',
+  },
+  cartItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 5,
+  },
+  cartItemName: {
+    fontSize: 16,
+    color: 'white',
+  },
+  cartItemPrice: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  cartTotal: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'right',
+    marginTop: 10,
+    color: 'white',
+  },
+  backButton: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+  },
+});
--- src/components/Comp Home/AdicionarSaldo.js
+++ src/components/Comp Home/AdicionarSaldo.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { View, Text, Button, StyleSheet, TouchableOpacity, Image } from 'react-native'
+import { View, Text, Button, StyleSheet, TouchableOpacity, Image, TextInput } from 'react-native'
+import { useState } from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default  function AdicionarSaldo( {handle} ) {
+  const [saldo, setSaldo] = useState();;
   return (
     <View style={styles.Container}>
       <Image source={require('../Comp Você/imagebg.png')} style={styles.backgroundImage} />
@@ -10,6 +12,11 @@ export default  function AdicionarSaldo( {handle} ) {
         <View style={{ backgroundColor: 'red', borderRadius: 50, width: 40, height: 40, justifyContent: 'center', alignItems: 'center' }}>
           <Icon name="arrow-left" size={20} color="white" />
         </View>
+      </TouchableOpacity>
+      <Text style={{ color: 'white', fontSize: 20, marginTop: 20 }}>Informe o valor do saldo a ser adicionado:</Text>
+      <TextInput value={saldo} onChangeText={(text) => setSaldo(text)} keyboardType='numeric' textContentType='telephoneNumber' style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1, marginTop: 20, padding: 10, color: "white" }} placeholder='R$00,00' placeholderTextColor={'white'}/>
+      <TouchableOpacity>
+        <Text style={{ color: 'white', fontSize: 20, marginTop: 20, backgroundColor: 'red', padding: 10, width: 300, textAlign: 'center' }}>Adicionar</Text>
       </TouchableOpacity>
     </View>
   )
